1777d47149a954697809d5a970f3c8e045cf0f86
Oonanongprawong <baitoey.24062542@gmail.com>
2018-05-11 15:40:08 +0700

    message

--- New.py
+++ New.py
@@ -1,3 +1,18 @@
+class Student :
+
+    Data = []
+    key = None
+    def __init__(self,DATA):
+        self.Data.append(DATA)
+
+    def insert_student(self,key,newDATA):
+        self.Data.insert(key,newDATA)
+    def delete_student(self,key):
+        self.Data.remove(key)
+    def _student(self):
+        return self.Data
+
+
 class data:
     name = None
     lastname = None
@@ -6,13 +21,14 @@ class data:
     grad = None
     id = None
 
-    def __init__(self,ID=None,NAME = None,lastname = None,brithday = None,age = None,grad = None):
+    def __init__(self,ID=None,NAME = None,LASTNAME = None,BRITHDAY = None,AGE = None,GRAD = None):
         self.id = ID
         self.name = NAME
-        self.lastname = lastname
-        self.brithday = brithday
-        self.age = age
-        self.grad = grad
+        self.lastname = LASTNAME
+        self.brithday = BRITHDAY
+        self.age = AGE
+        self.grad = GRAD
+        '''
         out = open("datastudent.txt", "a", encoding="utf8")
         out.write(self.id)
         out.write(" ")
@@ -27,52 +43,70 @@ class data:
         out.write(self.grad)
         out.write(" ")
         out.write("\n")
-        out.close()
+        out.close()'''
     pass
+
+    def import_data(self):
+        self.id = str(input("กรอกรหัสนักเรียน"))
+        self.name = str(input("กรอกรายชื่อนักเรียน"))
+        self.lastname = str(input("กรอกนามสกุล"))
+        self.brithday = str(input("กรอกวัน/เดือน/ปี"))
+        self.age = str(input("กรอกอายุ"))
+        self.grad = str(input("กรอกเกรด"))
+        tmp = (self.id,self.name,self.lastname,self.brithday,self.age,self.grad)
+        tmp = Student(tmp)
+        print(tmp._student())
+
+    def read_data(self):
+        for i in range(len(Student.Data)):
+            print([i+1]," : ",Student.Data[i])
+    def delete_data(self,key):
+        tmp = Student.delete_student(Student,Student.Data[key+1])
+
+    def insert_data(self,key):
+        self.id = str(input("กรอกรหัสนักเรียน"))
+        self.name = str(input("กรอกรายชื่อนักเรียน"))
+        self.lastname = str(input("กรอกนามสกุล"))
+        self.brithday = str(input("กรอกวัน/เดือน/ปี"))
+        self.age = str(input("กรอกอายุ"))
+        self.grad = str(input("กรอกเกรด"))
+        dd = (self.id, self.name, self.lastname, self.brithday, self.age, self.grad)
+        tmp = Student.insert_student(Student,key,dd)
+        self.delete_data(key)
 
     def __str__(self):
         return "{},{},{},{},{},{}".format(self.id,self.name,self.lastname,self.brithday,self.age,self.grad)
 
-class Student(object):
-
-    Data = []
-    def __init__(self):
-        pass
-
-    def input_student(self):
-        return self.Data
-    pass
 
 
-def format_data(ID,NAME,LASTNAME,BRITHDAY,AGE,GARD):
-    s = Student()
-    p1 = (ID,NAME,LASTNAME,BRITHDAY,AGE,GARD)
-    s.Data.append(p1)
-    big_data.append(p1)
-    print(s.input_student())
 
-def import_data():
-    ID = str(input("กรอกรหัสนักเรียน"))
-    NAME = str(input("กรอกรายชื่อนักเรียน"))
-    LASTNAME = str(input("กรอกนามสกุล"))
-    BRITHDAY = str(input("กรอกวัน/เดือน/ปี"))
-    AGE = str(input("กรอกอายุ"))
-    GARD = str(input("กรอกเกรด"))
-    return format_data(ID, NAME, LASTNAME, BRITHDAY, AGE, GARD)
+
+
 big_data = []
 while(1):
-    menu = int(input("ข้อมูลนักเรียน\nกดเมนูเพื่อเเข้าใช้งาน\nกด1.เพิ่มข้อมูลนักเรียน\nกด2.แก้ไขข้อมูลนักเรียน\nกด3.ลบข้อมูลนักเรียน"))
+    income = data()
+
+    menu = int(input("ข้อมูลนักเรียน\nกดเมนูเพื่อเเข้าใช้งาน\nกด1.เพิ่มข้อมูลนักเรียน\nกด2.แก้ไขข้อมูลนักเรียน\nกด3.ลบข้อมูลนักเรียน\nกด0.ส่งออกข้อมูลและจบโปรแกรม"))
     if menu == 1:
         num = int(input("กรอกจำนวนนักเรียน"))
         for c in range(num):
-            import_data()
+            income.import_data()
     elif menu == 0 :
-        for i in range(len(big_data)):
-            for n in range(len(big_data[i])):
+        for i in range(len(Student.Data)):
+            for n in range(len(Student.Data[i])):
                 out = open("outstudent.txt", "a", encoding="utf8")
-                out.write(str(big_data[i][n]))
+                out.write(str(Student.Data[i][n]))
                 out.write(" ")
             out.write('\n')
             out.close()
+        exit(0)
+    elif menu == 2 :
+        income.read_data()
+        n = int(input('ลำดับนักเรียนที่ต้องการแก้ไข'))
+        income.insert_data(n-1)
+    elif menu == 3 :
+        income.read_data()
+        n = int(input('ลำดับนักเรียนที่ต้องการลบ'))
+        income.delete_data(n-2)
 
 #แก้ไขข้อมูล ลบข้อมูล
